c46fcba26fcf589c361eb3ef2cda4ac4db801788
ajansari12 <65922160+ajansari12@users.noreply.github.com>
2025-12-21 10:49:44 -0500

    Updated constants.ts

--- constants.ts
+++ constants.ts
@@ -220,7 +220,7 @@ export const CASE_STUDIES: CaseStudy[] = [
       '400 new email subscribers in 2 months',
       'Calendar booked 6 weeks out'
     ],
-    image: '/elena_ross_1.jpg',
+    image: '/elena_ross.png',
     tags: ['Website', 'Automation'],
     stack: ['Webflow', 'ConvertKit', 'Framer Motion']
   },
